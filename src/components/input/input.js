f861f76f078848298864f5778b404cda9ca650ac
RostislavIvlev <56726375+RostislavIvlev@users.noreply.github.com>
2020-10-21 00:31:37 +0500

    Input (mixin, plugin for mask, styles, examples in devtool)

---
+++ src/components/input/input.js
@@ -0,0 +1,4 @@
+Inputmask({
+    alias: "datetime",
+    inputFormat: "dd.mm.yyyy",
+    placeholder: "ДД.ММ.ГГГГ"}).mask(".inputWrapper__input_date");
--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
+import '../node_modules/jquery/dist/jquery';
+let $ = require('jquery');
+
+import '../node_modules/inputmask/dist/jquery.inputmask.js'
+
+import './components/input/input'
+
+
+
 // Autoadd all scss/css/ts files
 function importAll (r) {
     r.keys().forEach(r);
 }
-  
 importAll(require.context('./', true, /\.scss|css|ts$/));
