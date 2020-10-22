ce7cf0e653484ed39ae1abdda7bc065d3d48c736
RostislavIvlev <56726375+RostislavIvlev@users.noreply.github.com>
2020-10-22 19:50:06 +0500

    rating button

---
+++ src/components/rating/rating.js
@@ -0,0 +1,5 @@
+$(function() {
+    $('.rating').barrating({
+      theme: 'css-stars',
+    });
+});
--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import '../node_modules/jquery/dist/jquery';
 let $ = require('jquery');
 
 import '../node_modules/inputmask/dist/jquery.inputmask.js'
+import '../node_modules/jquery-bar-rating/dist/jquery.barrating.min.js'
+import '../node_modules/jquery-bar-rating/dist/themes/css-stars.css'
 
 import './components/input/input'
 import './components/like/like'
+import './components/rating/rating'
+
 
 
 
