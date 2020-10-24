ee8ce069bbfc183dabd20f1b24e3302ebd339019
RostislavIvlev <56726375+RostislavIvlev@users.noreply.github.com>
2020-10-24 09:02:21 +0500

    fastChange for dropdown

--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -5,19 +5,35 @@ $(".dropdownWrapper__arrow").on("click", function() {
     $(this).siblings(".dropdown").toggleClass("dropdown_expanded");
     $(this).siblings(".dropdownContent").toggleClass("dropdownContent_expanded")
 })
+$(".dropdown").on("click", function() {
+    $(this).toggleClass("dropdown_expanded");
+    $(this).siblings(".dropdownWrapper__arrow").toggleClass("dropdownWrapper__arrow_expanded");
+    $(this).siblings(".dropdownContent").toggleClass("dropdownContent_expanded")
+})
 
-$(".contentLine__button_plus").on("click", function() {
+
+$("[data-plus]").on("click", function() {
     $(this).siblings(".contentLine__current").attr("data-current", function(index, attr) {
         attr < 6 ? attr++ : 6;
         return attr
     })
 })
-
-$(".contentLine__button_minus").on("click", function() {
+$("[data-minus]").on("click", function() {
     $(this).siblings(".contentLine__current").attr("data-current", function(index, attr) {
         attr > 0 ? attr-- : 0;
         return attr
     })
+})
+
+$("[data-fast]").on("click", function() {
+    let curr = $(this).closest(".dropdownContent").find(".contentLine__current");
+    let name = "";
+    curr.each(function(index, element){
+        let currentName = $(element).attr("data-name") + " " + $(element).attr("data-current") + "," + " ";
+        name += currentName;
+    });
+
+    $(this).closest(".dropdownWrapper").children(".dropdown").val(name);
 })
 
 $("[data-clear]").on("click", function() {
@@ -26,4 +42,26 @@ $("[data-clear]").on("click", function() {
         let base = $(element).attr("data-base");
         $(element).attr("data-current", base)
     })
+})
+$("[data-submit]").on("click", function() {
+    let curr = $(this).closest(".dropdownContent").find(".contentLine__current");
+    let summary = 0;
+    curr.each(function(index, element) {
+        let currentValue = parseInt($(element).attr("data-current"));
+        summary += currentValue
+    })
+
+    if (summary == 1) {
+        summary = summary + " гость";
+    } else if (summary > 1 && summary < 5) {
+        summary = summary + " гостя"
+    } else {
+        summary = summary + " гостей"
+    }
+     
+    $(this).closest(".dropdownWrapper").children(".dropdown").val(summary);
+
+    $(this).closest(".dropdownWrapper").children(".dropdownWrapper__arrow").removeClass("dropdownWrapper__arrow_expanded");
+    $(this).closest(".dropdownWrapper").children(".dropdown").removeClass("dropdown_expanded");
+    $(this).closest(".dropdownWrapper").children(".dropdownContent").removeClass("dropdownContent_expanded");
 })
