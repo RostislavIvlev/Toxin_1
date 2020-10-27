9d1b0758e45657328a7926623b4b1ed5e2da790f
RostislavIvlev <56726375+RostislavIvlev@users.noreply.github.com>
2020-10-28 01:46:32 +0500

    datepicker

---
+++ src/components/datepicker/datepicker.js
@@ -0,0 +1,65 @@
+var minDate = new Date();
+minDate.setDate(minDate.getDate() + 1);
+
+$("#startDate").datepicker({
+    prevHtml: '<div class="nav-arrow">arrow_back</div>',
+    nextHtml: '<div class="nav-arrow">arrow_forward</div>',
+    minDate: minDate,
+    onShow: function(d) {
+        if(d.$datepicker.find('button').html() === undefined ) {
+            d.$datepicker.append('<button type="button" class="datepicker--ready"> Применить </button>');
+            d.$datepicker.find('button').click(function(event) {
+                d.hide(); //Should be save data in databank + hide
+            });
+        }
+    },
+    onSelect: function(fd, d, picker) {
+        $("#startDate").val(fd.split("-")[0]);
+        $("#endDate").val(fd.split("-")[1]);
+    },
+    navTitles: {
+        days: 'MM yyyy'
+    },
+    clearButton: true,
+})
+
+$("#endDate").datepicker({
+    prevHtml: '<div class="nav-arrow">arrow_back</div>',
+    nextHtml: '<div class="nav-arrow">arrow_forward</div>',
+    position: 'bottom right',
+    minDate: minDate,
+    onShow: function(d) {
+        if(d.$datepicker.find('button').html() === undefined ) {
+            d.$datepicker.append('<button type="button" class="datepicker--ready"> Применить </button>');
+            d.$datepicker.find('button').click(function(event) {
+                d.hide(); //Should be save data in databank + hide
+            });
+        }
+    },
+    onSelect: function(fd, d, picker) {
+        $("#startDate").val(fd.split("-")[0]);
+        $("#endDate").val(fd.split("-")[1]);
+    },
+    navTitles: {
+        days: 'MM yyyy'
+    },
+    clearButton: true,
+});
+
+$("#filterDatePicker").datepicker({
+    prevHtml: '<div class="nav-arrow">arrow_back</div>',
+    nextHtml: '<div class="nav-arrow">arrow_forward</div>',
+    position: 'bottom right',
+    minDate: minDate,
+    dateFormat: "dd M",
+    startDate: new Date(),
+    onShow: function(d) {
+        if(d.$datepicker.find('button').html() === undefined ) {
+            d.$datepicker.append('<button type="button" class="datepicker--ready"> Применить </button>');
+            d.$datepicker.find('button').click(function(event) {
+                d.hide(); //Should be save data in databank + hide
+            });
+        }
+    },
+    clearButton: true,
+})
--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import '../node_modules/jquery/dist/jquery';
 let $ = require('jquery');
+import 'air-datepicker'
+
 
 import '../node_modules/inputmask/dist/jquery.inputmask.js'
 import '../node_modules/jquery-bar-rating/dist/jquery.barrating.min.js'
 import '../node_modules/jquery-bar-rating/dist/themes/css-stars.css'
+import '../node_modules/air-datepicker/dist/js/datepicker'
+import '../node_modules/air-datepicker/dist/css/datepicker.css'
 
+
+import './components/datepicker/datepicker'
 import './components/dropdown/dropdown'
 import './components/expand/expand'
 import './components/input/input'
 import './components/like/like'
 import './components/rating/rating'
-
 
 
 
