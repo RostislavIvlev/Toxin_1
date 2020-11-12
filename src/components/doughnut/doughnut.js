a05390ee8c1bf722eadc2adcd1eaa482eba46e6f
RostislavIvlev <56726375+RostislavIvlev@users.noreply.github.com>
2020-11-12 14:11:43 +0500

    doughnut

--- src/components/doughnut/doughnut.js
+++ src/components/doughnut/doughnut.js
@@ -1,45 +1,108 @@
-const { initial } = require("lodash");
+const { intersection } = require("lodash");
 
-function doughnut(data) {
-    var canvas = document.getElementById("doughnut");
-        canvas.width = 120;
-        canvas.height = 120;
+(function( $ ) {
+    $.fn.doughnut = function(data) {
+        var summary = 0;
+            angle = [];
+            currPosition = [-0.5*Math.PI];
+            colors = ["#FFE39C", "#6FCF97", "#BC9CFF", "#909090"];
 
-    var context = canvas.getContext("2d");
+        var calcSummary = function(data) {
+            summ = (x, y) => x + y;
+            summary = data.reduce(summ);
+            return summary;
+        };
 
-    var summary = 0;
-        angle = [];
-        position = [-0.5*Math.PI];
+        var calcAngle = function(data) {
+            data.forEach(function(item, i, data) {
+                angle[i] = item/summary*100*0.062831853071796;
+                return angle;
+            })
+        };
 
-        colors = ["#FFE39C", "#6FCF97", "#BC9CFF", "#909090"];
-
-    function buildDoughnut(data) {
-        summ = (x, y) => x + y;
-        summary = data.reduce(summ);
-        data.forEach(function(item, i, data) {
-            angle[i] = item/summary*100*0.062831853071796;
-        })
-        
-        for (let i = 1; i < angle.length; i++) {
-            position[i] = position[i-1] - angle[i-1];
-        }
-
-        context.lineWidth = 4;
-        position[4] = (-0.5*Math.PI);
-
-        for (let i = 0; i < data.length; i++) {
-            context.beginPath();
-            context.strokeStyle = colors[i];
-            if (data[i] == 0) {
-                context.arc(60, 60, 58, position[i], position[i], true);
-            } else {
-                context.arc(60, 60, 58, position[i], position[i+1] + 0.05, true);
+        var calcPosition = function(angle) {
+            for (let i = 1; i < angle.length; i++) {
+                currPosition[i] = currPosition[i-1] - angle[i-1];
             }
-            context.stroke();
+        };
+
+
+        var doughnutTemplate = function(target) {
+            let self = this;
+
+            self.container = $("<div/>").appendTo(target);
+            self.container.addClass("doughnutContainer");
+
+            self.doughnutWrapper = $("<div/>").appendTo(container);
+            self.doughnutWrapper.addClass("doughnutWrapper");
+
+            self.doughnutCanvas = $("<canvas/>").appendTo(doughnutWrapper);
+            self.doughnutCanvas.addClass("doughnutCanvas");
+
+            self.doughnutScore = $("<div/>").appendTo(doughnutWrapper);
+            self.doughnutScore.addClass("doughnutWrapper__score");
+
+            self.doughnutTranscript = $("<div/>").appendTo(container);
+            self.doughnutTranscript.addClass("doughnutTranscript");
+
+            var transcripts = ["Великолепно", "Хорошо", "Удовлетворительно", "Разочарован"];
+
+            for (let i = 0; i < transcripts.length; i++) {
+                self.transcriptLine = $("<div/>").appendTo(doughnutTranscript);
+                self.transcriptLine.addClass("transcriptLine");
+
+                self.elementColor = $("<div/>").appendTo(transcriptLine);
+                self.elementColor.addClass("transcriptLine__color transcriptLine__color_" + i);
+
+                self.elementText = $("<div/>").appendTo(transcriptLine);
+                self.elementText.addClass("transcriptLine__text");
+                self.elementText.text(transcripts[i]);
+            }
+        };
+
+        var doughnutBuild = function(data, target) {
+            calcSummary(data);
+            calcAngle(data);
+            calcPosition(data);
+
+            var canvas = $(".doughnutCanvas")[0];
+                canvas.width = 120;
+                canvas.height = 120;
+
+            var context = canvas.getContext("2d");
+
+            context.lineWidth = 4;
+            for (let i = 1; i < angle.length; i++) {
+                currPosition[i] = currPosition[i-1] - angle[i-1];
+            };
+
+            currPosition[4] = (-0.5*Math.PI);
+
+            for (let i = 0; i < data.length; i++) {
+                context.beginPath();
+                context.strokeStyle = colors[i];
+                if (data[i] == 0) {
+                    context.arc(60, 60, 58,currPosition[i],currPosition[i], true);
+                } else {
+                    context.arc(60, 60, 58,currPosition[i],currPosition[i+1] + 0.0174533, true);
+                }
+                context.stroke();
+            }
+
+
         }
 
+        var score = function(data) {
+            calcSummary(data);
+            $(".doughnutWrapper__score").text(summary);
+        }
+
+        doughnutTemplate(this);
+        doughnutBuild(data, this);
+        score(data)
 
     }
+})(jQuery)
 
-    buildDoughnut(data);
-}
+
+var $doughnut = $(".doughnut").doughnut([130, 65, 65, 1]);
